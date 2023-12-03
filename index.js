cf4e7a1f138236be877749c03d874395a9469ca4
Great-Stuff <124748279+Great-Stuff@users.noreply.github.com>
2023-12-03 16:06:29 -0500

    Add files via upload
    v2

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-function checkInterval(){ setInterval(() => {
-    const assignment = document.querySelector(".member-assignment-list-container")
-    const button = document.querySelector(".change-assignment-viewed-button")
-    button.onclick(checkInterval())
-    if (assignment) {
-        console.log("Assignments found:", assignment)
-        clearInterval(checkInterval);
-        const assignments = document.querySelectorAll(".member-assignment-list-container")
-        for (let i = 0; i < assignments.length; i++) {
-            assignments[i].style.width = "200%"
-            if (
-            String(assignments[i].innerHTML).includes("mild") ||
-            String(assignments[i].innerHTML).includes("Mild") ||
-            String(assignments[i].innerHTML).includes("espanol")
-            ){
-                assignments[i].style.display = "none"
+function main(){
+    const checkInterval = setInterval(() => {
+        const assignment = document.querySelector(".member-assignment-list-container")
+        const incompleteAssignmentsButton = document.querySelector("html.js.no-touch.svg.inlinesvg.svgclippaths.no-ie8compat body div#content-container div#content-area.content-wrap.row.no-padding div#app div div#group-container-main-wp div#group-assignments.groups-container.row.collapse div#group-details-container.large-9.small-12.columns.js-large-edit div#group-assignment-container.row.collapse.group-assignment-container div.small-12.large-12.columns.member-assignment-wrapper div.row.collapse.member-assignment-header.hide-for-small div.change-assignment-viewed.active-view.change-assignment-viewed-button")
+        if (assignment && incompleteAssignmentsButton) {
+            clearInterval(checkInterval)
+            console.log(`Assignment found`, assignment, incompleteAssignmentsButton)
+            const assignments = document.querySelectorAll(".member-assignment-list-container")
+            for (let i = 0; i < assignments.length; i++) {
+                if (
+                    String(assignments[i].innerHTML).includes("mild") ||
+                    String(assignments[i].innerHTML).includes("Mild") ||
+                    String(assignments[i].innerHTML).includes("espanol")
+                ) {
+                    assignments[i].style.display = "none"
+                }
             }
+            incompleteAssignmentsButton.addEventListener("click", () => {
+                const redoInterval = setInterval(() => {
+                    main()
+                    clearInterval(redoInterval)
+                }, 3000)
+            })
         }
-    }
-    else {
-        console.log("Assignments not found yet")
-    }
-}, 500);}
-
-checkInterval()
+        else {
+            console.log(`Assignments not found yet`)
+        }
+    }, 500)
+}
+main()
