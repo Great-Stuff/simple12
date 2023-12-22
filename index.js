913082a524b718f494592aa4498af03a8cefbb18
Great-Stuff <124748279+Great-Stuff@users.noreply.github.com>
2023-12-21 20:26:37 -0500

    Update index.js
    updated code to 2.2

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const mutationObserver = new MutationObserver(() => {
+    main()
+})
+
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+}
+
+const italic = "font-style: italic;"
+
 function main(){
     const checkInterval = setInterval(() => {
         const assignment = document.querySelector(".member-assignment-list-container")
-        const incompleteAssignmentsButton = document.querySelector("html.js.no-touch.svg.inlinesvg.svgclippaths.no-ie8compat body div#content-container div#content-area.content-wrap.row.no-padding div#app div div#group-container-main-wp div#group-assignments.groups-container.row.collapse div#group-details-container.large-9.small-12.columns.js-large-edit div#group-assignment-container.row.collapse.group-assignment-container div.small-12.large-12.columns.member-assignment-wrapper div.row.collapse.member-assignment-header.hide-for-small div.change-assignment-viewed.active-view.change-assignment-viewed-button")
-        if (assignment && incompleteAssignmentsButton) {
+        const incompleteAssignmentsButton = getElementByXpath("/html/body/div[5]/div[2]/div[1]/div/div/div[1]/div[4]/div[4]/div/div[1]/div[1]")
+        const completeAssignmentsButton = getElementByXpath("/html/body/div[5]/div[2]/div[1]/div/div/div[1]/div[4]/div[4]/div/div[1]/div[2]")
+        const newChange = document.querySelector("#newChange")
+        if (assignment && incompleteAssignmentsButton && completeAssignmentsButton) {
             clearInterval(checkInterval)
-            console.log(`Assignment found`, assignment, incompleteAssignmentsButton)
+            console.log("%c Assignments found", `${italic} color: lightgreen`)
             const assignments = document.querySelectorAll(".member-assignment-list-container")
             for (let i = 0; i < assignments.length; i++) {
                 if (
@@ -16,15 +28,17 @@ function main(){
                 }
             }
             incompleteAssignmentsButton.addEventListener("click", () => {
-                const redoInterval = setInterval(() => {
-                    main()
-                    clearInterval(redoInterval)
-                }, 3000)
+                mutationObserver.observe(newChange, {childList: true})
+            })
+            completeAssignmentsButton.addEventListener("click", () => {
+                mutationObserver.disconnect()
+                console.log("%c Mutation Observer Disconnected", `${italic} color: #5681c4`)
             })
         }
         else {
-            console.log(`Assignments not found yet`)
+            console.log("%c Searching for assignments", `${italic} color: yellow`)
         }
     }, 500)
 }
+
 main()
